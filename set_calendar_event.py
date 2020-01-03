15a5a5d425b033f3dd98b1917a9af45ca50ae9fd
marnec <rvrmarco@gmail.com>
2020-01-03 12:03:10 +0100

    sloppy merging of the two codebases

---
+++ set_calendar_event.py
@@ -0,0 +1,70 @@
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES += ['https://www.googleapis.com/auth/contacts.readonly']
+SCOPES += ['https://www.googleapis.com/auth/calendar']
+
+
+SAMPLE_SPREADSHEET_ID = '17-kJlVAcWF7q7hgC1z05kWnK7nipK_OrKkGzVlSZj8c'
+SAMPLE_RANGE_NAME = 'Next-Actions!A2:H'
+
+
+"""Shows basic usage of the Google Calendar API.
+Prints the start and name of the next 10 events on the user's calendar.
+"""
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+
+######## GET DATA FROM SHEETS ###########################
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=SAMPLE_RANGE_NAME).execute()
+
+values = result.get('values', [])
+print(values)
+
+
+####### INSERT DATA IN CALENDAR #########################
+
+service = build('calendar', 'v3', credentials=creds)
+
+# Call the Calendar API
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+print('Getting the upcoming 10 events')
+events_result = service.events().list(calendarId='primary', timeMin=now,
+                                    maxResults=10, singleEvents=True,
+                                    orderBy='startTime').execute()
+events = events_result.get('items', [])
+
+if not events:
+    print('No upcoming events found.')
+for event in events:
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    print(start, event['summary'])
+
