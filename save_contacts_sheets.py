e1b9817efabb191578be87fe11c8db6a0f1271b6
marnec <rvrmarco@gmail.com>
2020-01-03 08:21:54 +0100

    drafted modularization of authentication (not working)

--- save_contacts_sheets.py
+++ save_contacts_sheets.py
@@ -1,19 +1,8 @@
-import pickle
-import os.path
 from pprint import pprint
-
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
 
 ##################### SPREADSHEETS ###########################################
 
-# # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SCOPES += ['https://www.googleapis.com/auth/contacts.readonly']
-
-print(SCOPES)
 
 # The ID and range of a sample spreadsheet.
 # SAMPLE_SPREADSHEET_ID = '1xrAiKF3AwJtLr2Y1GUeCaZ4AfgkeH8mfg-6WQyTdjco'
@@ -24,24 +13,7 @@ SAMPLE_RANGE_NAME = 'Validations!F2:F'
 """Shows basic usage of the Sheets API.
 Prints values from a sample spreadsheet.
 """
-creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
+
 
 service = build('sheets', 'v4', credentials=creds)
 
@@ -57,30 +29,9 @@ values = result.get('values', [])
 ####################### CONTACTS #########################################
 
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
-
 """Shows basic usage of the People API.
 Prints the name of the first 10 connections.
 """
-creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
 
 service = build('people', 'v1', credentials=creds)
 
